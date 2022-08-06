77285d6c214c5f883d5ff8f2e256d844ba7882a6
Luke Pighetti <lukepighetti@gmail.com>
2022-08-06 19:26:50 -0400

    wip emotes

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   TextInput,
   Button,
   KeyboardAvoidingView,
+  Image,
 } from "react-native";
 
 import tmi from "tmi.js";
@@ -43,6 +44,7 @@ export default function App() {
   useEffect(() => {
     const messageHandler = (channel, tags, message, self) => {
       setChatMessages((prev) => [{ channel, tags, message }, ...prev]);
+      // console.log({ channel, tags, message });
     };
 
     if (isClientReady) {
@@ -77,14 +79,63 @@ export default function App() {
         inverted
         data={chatMessages}
         renderItem={({ item }) => {
+          const emotes = item.tags.emotes;
+          // emote-key: Array [replace "startIndex-endIndex"],
+
+          //       "425618": Array [
+          //         "48-50",
+          //         "52-54",
+          //         "56-58",
+          //         "60-62",
+          //       ],
+
+          //PogChamp =>> 42069
+          // |
+
+          // "Hey guys PogChamp whats up"
+          // [<Text>Hey guys </Text><Image emoteId={42069/><Text> whats up</Text>}]
+
+          // const elements = []; /// JSX elements, <Text> or <Image> based on splitMapJoin
+
+          var s = item.message;
+
+          for (const [emoteKey, replaceRanges] of Object.entries(
+            emotes ?? {}
+          )) {
+            for (const range of replaceRanges) {
+              const [startIndex, endIndex] = range.split("-");
+              s =
+                s.substring(0, startIndex) +
+                `|${emoteKey}|` +
+                s.substring(endIndex + 1);
+            }
+          }
+
+          console.log(s);
+
+          const emoteUrl = (emoteKey) =>
+            `https://static-cdn.jtvnw.net/emoticons/v1/${emoteKey}/1.0`;
+
           return (
-            <Text style={{ marginVertical: 2, marginHorizontal: 20 }}>
+            <Text
+              style={{
+                marginVertical: 4,
+                marginHorizontal: 20,
+                lineHeight: 24,
+              }}
+            >
               <Text
                 style={{ color: item.tags.color ?? "blue", fontWeight: "700" }}
               >
                 {item.tags["display-name"] ?? item.tags.username}:{" "}
               </Text>
-              <Text>{item.message.trim()}</Text>
+              <Image
+                style={{ width: 24, height: 24 }}
+                source={{
+                  uri: emoteUrl(425618),
+                }}
+              />
+              <Text>{s.trim()}</Text>
             </Text>
           );
         }}
@@ -173,5 +224,50 @@ const styles = StyleSheet.create({
 //     "user-id": "190872391",
 //     "user-type": null,
 //     "username": "mahamdi_amine",
+//   },
+// }
+
+// Object {
+//   "channel": "#lukepighetti",
+//   "message": "HeyGuys HeyGuys HeyGuys HeyGuys HeyGuys HeyGuys LUL LUL LUL LUL",
+//   "tags": Object {
+//     "badge-info": null,
+//     "badge-info-raw": null,
+//     "badges": null,
+//     "badges-raw": null,
+//     "client-nonce": "7255d9f108b523a8387c0b2ded2e1775",
+//     "color": null,
+//     "display-name": "playyboiizzy",
+//     "emote-only": true,
+//     "emotes": Object {
+//       "30259": Array [
+//         "0-6",
+//         "8-14",
+//         "16-22",
+//         "24-30",
+//         "32-38",
+//         "40-46",
+//       ],
+//       "425618": Array [
+//         "48-50",
+//         "52-54",
+//         "56-58",
+//         "60-62",
+//       ],
+//     },
+//     "emotes-raw": "30259:0-6,8-14,16-22,24-30,32-38,40-46/425618:48-50,52-54,56-58,60-62",
+//     "first-msg": false,
+//     "flags": null,
+//     "id": "29753d29-cead-4545-8cce-8981bacc8254",
+//     "message-type": "chat",
+//     "mod": false,
+//     "returning-chatter": false,
+//     "room-id": "463970907",
+//     "subscriber": false,
+//     "tmi-sent-ts": "1659826717418",
+//     "turbo": false,
+//     "user-id": "675719401",
+//     "user-type": null,
+//     "username": "playyboiizzy",
 //   },
 // }
