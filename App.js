a1c6753eeb4f79c20a7946a00a0a355c812c141d
Luke Pighetti <lukepighetti@gmail.com>
2022-08-06 19:28:23 -0400

    clean up codebase, add placeholder

--- App.js
+++ App.js
@@ -61,7 +61,12 @@ export default function App() {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+      }}
+    >
       <View
         style={{ padding: 10, flexDirection: "row", justifyContent: "center" }}
       >
@@ -105,6 +110,7 @@ export default function App() {
             onChangeText={setComposedMessage}
             onSubmitEditing={submitMessage}
             value={composedMessage}
+            placeholder="Send a message"
           />
           <View style={{ marginHorizontal: 10 }}>
             <Button onPress={submitMessage} title={"Send"}></Button>
@@ -115,63 +121,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-// flexDirection: column* | row
-// MainAxisAlignment -> justifyContent:
-//      flex-start* | flex-end | center | space-between | space-around | space-evenly
-// CrossAxisAlignment -> alignItems:
-//      stretch* | flex-start | flex-end | center | baseline
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // justifyContent: "flex-start",
-    // alignItems: "stretch",
-  },
-});
-
-// const tmi = require('tmi.js');
-// const client = new tmi.Client({
-// 	options: { debug: true },
-// 	identity: {
-// 		username: 'bot_name',
-// 		password: 'oauth:my_bot_token'
-// 	},
-// 	channels: [ 'my_channel' ]
-// });
-// client.connect().catch(console.error);
-// client.on('message', (channel, tags, message, self) => {
-// 	if(self) return;
-// 	if(message.toLowerCase() === '!hello') {
-// 		client.say(channel, `@${tags.username}, heya!`);
-// 	}
-// });
-
-//  {
-//   "channel": "#lukepighetti",
-//   "message": "tst",
-//   "tags": Object {
-//     "badge-info": null,
-//     "badge-info-raw": null,
-//     "badges": null,
-//     "badges-raw": null,
-//     "client-nonce": "ef793fc96db76a008747e53034b978bc",
-//     "color": "#1E90FF",
-//     "display-name": "Mahamdi_Amine",
-//     "emotes": null,
-//     "emotes-raw": null,
-//     "first-msg": true,
-//     "flags": null,
-//     "id": "25aa602c-1f88-4bcd-877a-bb609d569b65",
-//     "message-type": "chat",
-//     "mod": false,
-//     "returning-chatter": false,
-//     "room-id": "463970907",
-//     "subscriber": false,
-//     "tmi-sent-ts": "1659821151955",
-//     "turbo": false,
-//     "user-id": "190872391",
-//     "user-type": null,
-//     "username": "mahamdi_amine",
-//   },
-// }
